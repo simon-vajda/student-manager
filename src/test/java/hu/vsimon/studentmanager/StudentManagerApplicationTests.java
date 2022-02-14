909bab63802be53d20516786b9c9a4afafcfa63e
simon <simon.vajda23@gmail.com>
2022-02-14 20:10:05 +0100

    Initial commit

---
+++ src/test/java/hu/vsimon/studentmanager/StudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.vsimon.studentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
