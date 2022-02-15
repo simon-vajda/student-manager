306e948cca16d421d391f8d13525efe257aa5296
simon <simon.vajda23@gmail.com>
2022-02-16 00:08:58 +0100

    feature: updating student data

--- src/main/java/hu/vsimon/studentmanager/student/StudentController.java
+++ src/main/java/hu/vsimon/studentmanager/student/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -23,17 +24,24 @@ public class StudentController {
         return "index";
     }
 
-    @GetMapping("/new-student")
+    @GetMapping("/new_student")
     public String newStudentPage(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
 
-        return "new_student";
+        return "student";
     }
 
-    @PostMapping("/new-student")
-    public String saveNewStudent(@ModelAttribute("student") Student student) {
+    @PostMapping("/save")
+    public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
         return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editStudentPage(@PathVariable long id, Model model) {
+        Student student = studentService.getStudentById(id);
+        model.addAttribute("student", student);
+        return "student";
     }
 }
